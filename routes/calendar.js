39553fb1a8d135f4187e2fbcef7d62395223b387
Gianmarco Moretti <moretti.gianmarco@gmail.com>
2025-03-08 18:00:29 +0100

    fix calendar next-race request

--- routes/calendar.js
+++ routes/calendar.js
@@ -85,7 +85,7 @@ router.get('/championship/:championship_id/next-race', authMiddleware, async (re
         qualifications_time,
         sprint_time,
         event_time,
-        race_id(name,location,country`)
+        race_id(name,location,country)`)
       .eq('championship_id', championshipId)
       .gte('event_date', today)
       .order('event_date', { ascending: true })
