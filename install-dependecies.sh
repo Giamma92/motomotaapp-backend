b72bfecacb6bf4c78d5ea2faec4207d4e49cdaec
Gianmarco Moretti <moretti.gianmarco@gmail.com>
2025-03-16 21:51:31 +0100

    fix dependecies

---
+++ install-dependecies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt-get update && apt-get install -y \
+    libnss3 \
+    libatk1.0-0 \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxfixes3 \
+    libxi6 \
+    libxrandr2 \
+    libgbm1 \
+    libgtk-3-0 \
+    libasound2
